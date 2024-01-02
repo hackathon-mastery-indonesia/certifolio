d02d42094034ed57c7f5cd583f36ffe8a0e01d4d
isaui <isadestroyed17@gmail.com>
2024-01-03 02:03:45 +0700

    dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,18 +7,52 @@ import Head from 'next/head';
 import CertificateCard from '../components/certificate_card';
 import {IMG_LINK_DUMMY, IMG_LINK_DUMMY_2} from '../dummy/img_link';
 import { IoMdAdd } from "react-icons/io";
-
+import { Alert, Snackbar, sna } from '@mui/material';
+import { useRouter } from 'next/navigation';
+import { SyntheticEvent, useEffect, useState } from 'react';
+import { RootState } from '@/util/redux/store/store';
+import { useAppSelector } from '@/util/redux/hooks/hooks';
 
 
 
 export default function Page() {
-    let isEmpty = true;
+
+    const params = new URLSearchParams(window.location.search);
+    const welcome = params.get('welcome');
+    const auth = useAppSelector((state: RootState)=> state.auth);
+
+    
+    const [open, setOpen] = useState(false);
+
+    useEffect(()=> {
+        if(!welcome){
+            return;
+        }
+        setOpen(true)
+
+
+
+    }, [welcome]);
+
+    const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+        setOpen(false);
+      };
+
+    let isEmpty = false;
     return (
         <main className="flex bg-gradient-to-b from-slate-950 to-slate-900 via-gray-950 min-h-screen flex-col items-center justify-center px-6 pt-20 md:pt-12 ">
             <LoginNav />
             <Head>
                 <title>Dashboard</title>
             </Head>
+            <Snackbar open={open} autoHideDuration={6000}  anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
+        <Alert severity="success" onClose={handleClose}>
+          Hello, {auth.username || 'Anonim'}
+        </Alert>
+      </Snackbar>
             <div className="mb-auto grow max-w-5xl w-[90vw] md:mt-6  min-w-64 flex mx-auto flex-col items-center justify-start  md:px-4">
             <div className=" flex justify-between mt-4 w-full b items-center px-4 py-2">
                 <h1 className="text-white text-xl font-semibold">Your Certificate</h1>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import { CustomizableNav} from '../components/partials/navbar';
 import {FaWallet} from 'react-icons/fa';
 import { loginUser } from '../../util/function/auth_util';
 
-import React, {  } from 'react';
+import React, { useState } from 'react';
 import User from '../../util/next_models/user';
 import {  useAppDispatch} from '../../util/redux/hooks/hooks';
 import { login } from '../../util/redux/features/auth_slice';
@@ -13,6 +13,7 @@ import { useRouter } from 'next/navigation';
 export default function Page() {
     const dispatch = useAppDispatch();
     const router = useRouter();
+    const [username, setUsername] = useState('')
 
     const setupLogin = async () => {
         try {
@@ -21,11 +22,11 @@ export default function Page() {
                     console.log(user)
                     console.log('HEREEE')
                     dispatch(login(user))
-                }
+                }, username
             );
             if(res == "SUCCESS"){
                 console.log('Sign In Confirmed');
-               // router.push('/');
+                router.push(`/dashboard?welcome=true`);
             }
             else {
                 throw Error('LOGIN FAILED DUE TO CANISTER ERROR');
@@ -47,7 +48,11 @@ export default function Page() {
                 <div className=" rounded-lg p-8 md:min-w-96">
                     <h2 className="text-2xl md:text-3xl font-bold mb-4 text-white text-center">Login to Certifolio</h2>
                     <div className="mb-4">
-                            <input required type="text" id="username" name="username" placeholder='Pick an Username' className="border border-neutral-700 rounded-md w-full text-white px-3 py-3 mt-1 bg-neutral-900" />
+                            <input required type="text" id="username" value={username} onChange={
+                                (event)=> {
+                                    setUsername(event.target.value)
+                                }
+                            } name="username" placeholder='Pick an Username' className="border border-neutral-700 rounded-md w-full text-white px-3 py-3 mt-1 bg-neutral-900" />
                         </div>
 
                     <button onClick={setupLogin} className="bg-white w-full justify-center flex items-center text-blue-900 font-bold rounded-md px-4 py-3 hover:bg-indigo-300 transition duration-300">
