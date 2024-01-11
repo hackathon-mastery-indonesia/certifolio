925497e0544bfa7837d350dd4f8175e86335bc20
isaui <isadestroyed17@gmail.com>
2024-01-11 23:52:08 +0700

    dev server

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,5 +1,4 @@
 echo "CBKADAL";
-
 sh -ci "$(curl -fsSL https://internetcomputer.org/install.sh)";
 #!/bin/bash
 sudo apt-get update;
@@ -9,30 +8,20 @@ locate libunwind.so.8;
 echo "HEY TAYO";
 # Install dependensi frontend
 npm install;
-
 echo "HEHEY INSTAL NPM";
-
 # Start dfx in background mode
 dfx start --background;
-
 echo "RUN BACKGROUND";
-
 # Change directory to backend
 cd certifolio_backend;
-
 # Install dependensi backend
 npm install;
-
 # Deploy with dfx
 dfx deploy;
-
 echo "SIP DEK";
-
 # Copy and override env from current path to parent
 cp -f .env ../.env;
-
 # Back to main directory
 cd ..;
-
 # Run npm run dev
-npm run build;
+#npm run build;
